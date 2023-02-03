0d94197ea2f80bd2c045388778083e867b11f1f2
ecanvasser <eddiecanvassere9@gmail.com>
2023-02-03 03:02:34 -0800

    Pass messages data to index.ejs

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
+const messages = [
+  { text: "Howdy partner", user: "John", added: new Date() },
+  { text: "Chalom", user: "Ben", added: new Date() },
+];
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { messages: messages });
 });
 
 module.exports = router;
